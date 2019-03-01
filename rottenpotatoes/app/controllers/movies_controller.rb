b86afd43012a96de5e4597d98571ae844cab2363
Zheyong Sun <sunzheyong@outlook.com>
2019-03-01 15:49:26 +0000

    part1

--- rottenpotatoes/app/controllers/movies_controller.rb
+++ rottenpotatoes/app/controllers/movies_controller.rb
@@ -1,13 +1,13 @@
 class MoviesController < ApplicationController
   
   def movie_params
-    params.require(:movie).permit(:title, :rating, :description, :release_date)
+    params.require(:movie).permit(:title, :rating, :description, :release_date, :director)
   end
 
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
-    # will render app/views/movies/show.<extension> by default
+    # will render app/views/movies/show.<extension> by defaultrake db:test:prepare
   end
 
   def index
